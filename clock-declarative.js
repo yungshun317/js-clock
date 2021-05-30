a4a2798814119d2d2174290887dac13b0abbbe05
yungshun317 <seraph317@gmail.com>
2021-05-30 19:25:09 +0800

    [FEAT] Create functions for mutating `Date` objects

--- clock-declarative.js
+++ clock-declarative.js
@@ -4,3 +4,19 @@ const getCurrentTime = () => new Date();
 const clear = () => console.clear();
 const log = message => console.log(message);
 
+// [2] Functions for mutating the Date object
+const serializeClockTime = date => ({
+	hours: date.getHours(),
+	minutes: date.getMinutes(),
+	seconds: date.getSeconds()
+});
+
+const civilianHours = clockTime => ({
+	...clockTime,
+	hours: clockTime.hours > 12 ? clockTime.hours - 12 : clockTime.hours
+});
+
+const appendAMPM = clockTime => ({
+	...clockTime,
+	ampm: clockTime > 12 ? "PM" : "AM"
+});
