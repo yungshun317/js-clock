cab2957df40772bf578a942f3ad896377634d272
yungshun317 <seraph317@gmail.com>
2021-05-30 19:16:50 +0800

    [FEAT] Create functions for obtaining values

---
+++ clock-declarative.js
@@ -0,0 +1,6 @@
+// [1] Functions for obtaining values
+const oneSecond = () => 1000;
+const getCurrentTime = () => new Date();
+const clear = () => console.clear();
+const log = message => console.log(message);
+
